a6bcad27eef5a2275da58bb553a4624adce67011
Fabian996 <blauerdrache2@aol.de>
2015-09-26 10:37:43 +0200

    Create ClearPrivateChat.java

---
+++ src/ClearChat/Commands/ClearPrivateChat.java
@@ -0,0 +1,33 @@
+package de.Fabian.SClearChat.Commands;
+
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import de.Fabian.SClearChat.MessagesConfig.MessageHandler;
+
+public class ClearPrivateChat implements CommandExecutor 
+	{
+	public final Logger logger = Logger.getLogger("Minecraft");
+
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLable, String[] args) {
+		if ((sender instanceof Player)) {
+		 Player player = (Player)sender;
+
+		 if (commandLable.equalsIgnoreCase("lcc"))
+		 if ((player.hasPermission("clearchat.lcc"))) 
+		 {
+		    for(int i = 0; i <= 100; i++) 
+		    { player.sendMessage("");}
+		      player.sendMessage(ChatColor.WHITE + "[" + ChatColor.GOLD + "ClearChat" + ChatColor.WHITE + ChatColor.AQUA +  "Du hast deinen eignen Chat gelöscht");
+		 }		       
+		 else 
+		 {sender.sendMessage(MessageHandler.getMessage("console-use-command"));}
+		}
+		return false;	
+     }	    
+}
